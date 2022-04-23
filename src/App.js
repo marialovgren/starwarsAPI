2444e3988c5322224b7f8ff3d855e390e757908f
marialovgren <marialovgren86@hotmail.com>
2022-04-23 20:03:33 +0200

    imported bootstrap element comtainer, route and routes from react-router-dom and routes to pages within app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import Navigation from './components/Navigation';
+import Container from 'react-bootstrap/Container'
+import { Routes, Route } from 'react-router-dom'
+
+// imported pages
+import HomePage from './pages/HomePage'
+import SearchStarwars from './pages/SearchStarwars'
+import NotFound from './pages/NotFound'
 
 // styles
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-     
-    </div>
-  );
+	return (
+		<div className="App">
+      		<Navigation />
+
+			<Container className="py-3">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/search" element={<SearchStarwars />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Container>
+    	</div>
+  	);
 }
 
 export default App;
