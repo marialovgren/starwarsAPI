be2a3f495da655c30d95a8a73c5ef91dab9d9708
marialovgren <marialovgren86@hotmail.com>
2022-04-24 08:25:46 +0200

    managed to fetch films and output them in the browser using films.results.map instead of just films.map

--- src/pages/Films.js
+++ src/pages/Films.js
@@ -2,14 +2,16 @@ import StarwarsAPI from '../services/SWAPI'
 import { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card'
 
+
 const Films = () => {
-    const [films, setFilms] = useState([])
+    const [films, setFilms] = useState("")
 
     // Get films from API
 	const getFilms = async () => {
 		const data = await StarwarsAPI.getFilms()
 		setFilms(data)
-      
+        console.log(data.results)
+        console.log(data)
 	}
 
     // Get films from API when component is first mounted
@@ -17,35 +19,27 @@ const Films = () => {
 		getFilms()
 	}, [])
 
-    console.log(films)
+   
 
     return (
         <div className="contentWrapper">
 			<h1>Films</h1>
 			<div className="cardWrapper">
 
-                {films.length > 0 && (
-                    <Card className="card">
-					    {films.map(film => 
-                            <Card.Body>
-                                <Card.Title 
-                                    action
-                                    key={film.id}
-                                >
-                                    {film.title}
-                                </Card.Title>
+            {films && films.results.map((films) => (
+                <Card className="card"> 
+                    <Card.Header>{films.title}</Card.Header>
+                    <Card.Body>
+                        <Card.Title>
+                            Eposode id: {films.episode_id}
+                        </Card.Title>          
+                        <Card.Text>
+                            {films.opening_crawl}
+                        </Card.Text>
+                    </Card.Body>
+				</Card>
+            ))}
 
-                                <Card.Text>
-                                    <p className="small mb-0">{film.episode_id}</p>
-                                </Card.Text>
-                            </Card.Body>
-                        )}
-				    </Card>
-                )}
-
-                {films.length === 0 && (
-				    <p className="status">No films</p>
-			    )}
 			</div>
 		</div>
   )
